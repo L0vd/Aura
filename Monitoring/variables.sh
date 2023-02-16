580f9cb8d17d8042db3ebcf94ceba8344e6c0475
NktSH <49846495+NktSH@users.noreply.github.com>
2023-02-17 01:16:53 +0500

    Add files via upload

---
+++ Monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Aura monitoring variables template 
+COS_BIN_NAME=insert_path_to_aura_binary # example: /root/go/bin/aurad or /home/user/go/bin/aurad
+COS_PORT_RPC=insert_port_here              # default: 26657
+COS_VALOPER=insert_valoper_address         # example: auravaloper1234545636767376535673
+logentry=aura
